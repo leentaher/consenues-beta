68634c00caab85e1cbabab9cf2628214db568e2c
Leen Al-Taher <leen.taher@gmail.com>
2026-04-15 15:31:38 -0600

    add oauth handler

--- api/auth.js
+++ api/auth.js
@@ -1,20 +1,7 @@
-const CLIENT_ID = 'Ov23lilWuf4RoPUbZcp5';
-const CLIENT_SECRET = '652fbbe782ad67968306f59f0b0fc6df199c1e06';
-
 export default async function handler(req, res) {
-  const { code } = req.query;
-  if (!code) {
-    return res.redirect(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=repo`);
-  }
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-    body: JSON.stringify({ client_id: CLIENT_ID, client_secret: CLIENT_SECRET, code })
-  });
-  const data = await response.json();
-  const token = data.access_token;
-  res.send(`<script>
-    window.opener.postMessage('authorization:github:success:{"token":"${token}","provider":"github"}', '*');
-    window.close();
-  </script>`);
+  const {code} = req.query;
+  if (!code) return res.redirect(`https://github.com/login/oauth/authorize?client_id=Ov23lilWuf4RoPUbZcp5&scope=repo`);
+  const r = await fetch("https://github.com/login/oauth/access_token", {method:"POST",headers:{"Content-Type":"application/json","Accept":"application/json"},body:JSON.stringify({client_id:"Ov23lilWuf4RoPUbZcp5",client_secret:"652fbbe782ad67968306f59f0b0fc6df199c1e06",code})});
+  const d = await r.json();
+  res.send(`<script>window.opener.postMessage("authorization:github:success:{\"token\":\"${d.access_token}\",\"provider\":\"github\"}", "*");window.close();<\/script>`);
 }
